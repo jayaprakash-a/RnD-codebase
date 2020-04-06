58fb1285ca30ef381cf943097086fc8ddfb9d8b9
Jayaprakash A <akulajayaprakash@gmail.com>
2020-04-06 13:15:44 +0530

    First commit

--- Resemblyzer/speaker_classification.py
+++ Resemblyzer/speaker_classification.py
@@ -20,19 +20,31 @@ def preprocess_wavs():
 	np.save('preprocess_wav_files.npy', wavs)
 
 def loadFiles():
-	wavs = np.load('preprocess_wav_files.npy', allow_pickle=True)
+	print('loading starts')
+	#wavs = np.load('preprocess_wav_files.npy', allow_pickle=True)
 	## Compute the embeddings
 	encoder = VoiceEncoder()
 	#utterance_embeds = np.array(list(map(encoder.embed_utterance, wavs)))
 
 	#np.save('utterance_embeds.npy', utterance_embeds)
-	wavs = np.load('utterance_embeds.npy', allow_pickle=True)
+	utterance_embeds = np.load('utterance_embeds.npy', allow_pickle=True)
+	print('loading finished')
+	path = "/home/data/ToysFromTrash/audio/"
+	wav_fpaths = list(glob.glob(os.path.join(path, '*.wav')))
+	print(len(wav_fpaths))
+	return utterance_embeds
 
-def plot():
+def plot(utterance_embeds):
 	## Project the embeddings in 2D space
+	print('plot the graph')
+	print(len(utterance_embeds))
 	plot_projections(utterance_embeds, title="Embedding projections")
 	fig1 = plt.gcf()
 	plt.show()
 	plt.draw()
 	fig1.savefig('plot.png', dpi=100)
 	#plt.show()
+def main():
+	utterance_embeds = loadFiles()
+	plot(utterance_embeds)
+main()
